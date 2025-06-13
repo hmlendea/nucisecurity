634cf043b835c2eb2e9de3ba14dc87641bf2bcbc
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-06-13 11:35:54 +0300

    generic: Reworked HMAC Encoder to be static and handle any generic object

--- IHmacEncoder.cs
+++
@@ -1,9 +0,0 @@
-namespace NuciSecurity.HMAC
-{
-    public interface IHmacEncoder<T> where T : class
-    {
-        string GenerateToken(T obj, string sharedSecretKey);
-
-        bool IsTokenValid(string expectedToken, T obj, string sharedSecretKey);
-    }
-}
